35dd5f902d135a067f07f1001b7d87532d647511
joaco <mellado.joaquin@gmail.com>
2020-08-12 19:30:22 -0400

    fourth commit fixing errors from desktop adding more features

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,33 @@
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-  })
+  });
 
 
+// smooth scrolling function 
 
+  $(document).ready(function(){
+    $("a").on('click', function(event) {
+  
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 100, function(){
+  
+          window.location.hash = hash;
+        });
+      } 
+    });
+  });
+
+  // Carousel
+
+  $('.carousel').carousel({
+    interval: 3000,
+    pause: false,
+    wrap: true
+})
