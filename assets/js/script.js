95ae740e92882e04a997e3ca97336d8e3829f0ce
joaco <mellado.joaquin@gmail.com>
2020-08-15 01:42:36 -0400

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,42 @@ $(function () {
     pause: false,
     wrap: true
 })
+
+
+// Handling events 
+$(document).ready(function(){
+  $('#name').click(function() {
+      $('#name').animate({
+        left: 800,
+        right: -800
+      }, 200);
+      $('#name').animate({
+        left: -500,
+        top: 300
+      }, 200);
+      $('#name').animate({
+        left: 0,
+        top: 0
+      }, 200);
+    });
+});
+
+$(document).ready(function(){
+  $('#iam').click(function() {
+      $('#iam').animate({
+        top: -500
+      }, 200);
+      $('#iam').animate({
+        left: 0,
+        top: 300
+      }, 200);
+      $('#iam').animate({
+        left: 0,
+        top: 0
+      }, 200);
+    });
+});
+
+
+
+       
